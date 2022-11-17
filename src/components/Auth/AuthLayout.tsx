125755206b7f062044b34343f21d832bd68df4e0
Liza Zavalko <asklepia2012@gmail.com>
2022-11-17 11:16:55 +0200

    EXPA-03 ADD: create layout for authorization

---
+++ src/components/Auth/AuthLayout.tsx
@@ -0,0 +1,38 @@
+import { CardContent, Typography} from "@mui/material"
+import { PropsWithChildren } from "react"
+import { AuthCard } from "../../styles/AuthCard"
+import LogoSVG from '../../images/auth/Logo.svg'
+import { Logo } from "../../styles/Logo"
+import { AuthMainImg } from "../../styles/AuthMainImg"
+
+interface Props extends PropsWithChildren { 
+  title: string,
+  alt: string,
+  mainImg: string,
+};
+
+export const AuthLayout: React.FC<Props> = (props) => {
+  const {
+    title,
+    alt,
+    mainImg,
+  } = props;
+  return (
+    <AuthCard>
+      <CardContent sx={{ width: '50%' }}>
+        <Logo alt="logo" image={LogoSVG} />
+
+        <Typography variant="h1" color="primary.light" sx={{ mb: '32px' }}>
+          {title}
+        </Typography>
+
+        {props.children}
+      </CardContent>
+
+      <img
+        alt={alt}
+        src={mainImg}
+      />
+    </AuthCard>
+  )
+}
