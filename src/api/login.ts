a5a31158dc6035232a2ec3cd1d0c6ac7c02d60e6
Liza Zavalko <asklepia2012@gmail.com>
2022-11-18 12:13:45 +0200

    EXPA-03 ADD: login api

---
+++ src/api/login.ts
@@ -0,0 +1,19 @@
+import { setFeaturesTokenActionCreator } from '../redux/features'
+import { login } from './auth'
+
+interface Props {
+  username: string
+  password: string
+}
+
+export const handleLogin = async (props: Props) => {
+  try {
+    const token = await login(props.username, props.password)
+    console.log('my token', token)
+    if (token) {
+      setFeaturesTokenActionCreator(token.accessToken)
+    }
+  } catch (err) {
+    console.log('my err', err)
+  }
+}
