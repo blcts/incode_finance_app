97b38e8ec9c7566fcf40fbd24d7cf3c219daf3dd
Liza Zavalko <asklepia2012@gmail.com>
2022-11-21 12:23:22 +0200

    EXPA=04 UPD: fixed bug in axios

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,8 +2,6 @@ import Axios from 'axios'
 
 export const axios = Axios.create({
   baseURL: 'https://incode-backend-dev.herokuapp.com',
-  withCredentials: true,
-  headers: { 'Content-Type': 'application/json;' },
 })
 
 axios.interceptors.response.use(
