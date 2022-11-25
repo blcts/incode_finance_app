b68e7043a5299d26c813287fbafc35dea0421dab
Liza Zavalko <asklepia2012@gmail.com>
2022-11-25 17:30:30 +0200

    EXPA-11 UPD: change way to auth w/ refresh

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -1,5 +1,27 @@
+import { useEffect, useState } from 'react'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+import { axios } from '../api/axios'
+import { getRefreshToken } from './fetch'
 import { AppDispatch, RootState } from './store'
 
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export const useAuth = () => {
+  const [token, setToken] = useState<string | null>(null)
+  const refreshToken = useAppSelector(getRefreshToken)
+
+  useEffect(() => {
+    ;(async () => {
+      if (!refreshToken) return
+
+      const newToken = (await axios.post('/auth/refresh', {
+        refreshToken,
+      })) as string
+      if (newToken) {
+        setToken(newToken)
+      }
+    })()
+  }, [refreshToken])
+
+  return token
+}
--- src/routes/AppRouting.tsx
+++ src/routes/AppRouting.tsx
@@ -1,20 +1,27 @@
-import { RouteObject, useRoutes } from "react-router-dom";
-import { getToken } from "../redux/fetch";
-import { useAppSelector } from "../redux/hooks";
-import { authRouting } from "./AuthRouting";
-import { sidebarRouting } from "./SidebarRouting";
+import { useDispatch } from 'react-redux'
+import { RouteObject, useRoutes } from 'react-router-dom'
+import { axios } from '../api/axios'
+import {
+  getRefreshToken,
+  getToken,
+  setRefreshToken,
+  setToken,
+} from '../redux/fetch'
+import { useAppSelector, useAuth } from '../redux/hooks'
+import { authRouting } from './AuthRouting'
+import { sidebarRouting } from './SidebarRouting'
 
 export const AppRouting = () => {
-  const routes: RouteObject[] = [];
-  const token = useAppSelector(getToken);
+  const routes: RouteObject[] = []
+  const token = useAuth()
 
   if (token) {
     routes.push(...sidebarRouting)
   } else {
-    routes.push(...authRouting);
+    routes.push(...authRouting)
   }
-  
-  const element = useRoutes(routes);
 
-  return <>{ element }</>;
+  const element = useRoutes(routes)
+
+  return <>{element}</>
 }
